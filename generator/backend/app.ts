157a2f4516ea8809c607136846c427bdd8e90c70
BalintB4 <balazsbalint513@gmail.com>
2025-03-10 19:05:06 +0100

    update

--- generator/backend/app.ts
+++ generator/backend/app.ts
@@ -7,38 +7,41 @@ import { StatusCodes } from "http-status-codes";
 
 const app = express();
 const port = 3000;
-const dir = path.join(__dirname, 'public')
-var morgan = require('morgan')
+const dir = path.join(__dirname, "public");
+var morgan = require("morgan");
 
-app.get("/", (req, res) => {
+// Feste Base-URL
+const BASE_URL = "https://if200147.cloud.htl-leonding.ac.at";
+
+app.get(`${BASE_URL}/`, (req, res) => {
     res.sendFile(path.join(dir, "mendix-generator.html"));
 });
 
-app.use(express.static(dir));
+app.use(`${BASE_URL}`, express.static(dir));
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
-app.use("/api/generator", generatorRouter);
+app.use(`${BASE_URL}/api/generator`, generatorRouter);
 
-app.get("/mendix-generator.html", (req, res) => {
+app.get(`${BASE_URL}/mendix-generator.html`, (req, res) => {
     res.sendFile(path.join(dir, "mendix-generator.html"));
 });
 
-app.get("/generate-mendix-app.html", (req, res) => {
+app.get(`${BASE_URL}/generate-mendix-app.html`, (req, res) => {
     res.sendFile(path.join(dir, "generate-mendix-app.html"));
 });
 
-app.get("/index.html", (req, res) => {
+app.get(`${BASE_URL}/index.html`, (req, res) => {
     res.sendFile(path.join(dir, "index.html"));
 });
 
-app.get("/select-entities.html", (req, res) => {
+app.get(`${BASE_URL}/select-entities.html`, (req, res) => {
     res.sendFile(path.join(dir, "select-entities.html"));
 });
 
-app.get("/download-mpk.html", (req, res) => {
+app.get(`${BASE_URL}/download-mpk.html`, (req, res) => {
     res.sendFile(path.join(dir, "download-mpk.html"));
 });
 
@@ -51,12 +54,10 @@ app.use((req, res, next) => {
 app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
     console.error(err.stack);
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      message: 'Something went wrong!',
+        message: "Something went wrong!",
     });
-  });
-
-  app.listen(port, "0.0.0.0", () => {
-    console.log(`Server is listening at http://localhost:${port} (intern im Container)`);
 });
 
-
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server is listening at http://localhost:${port} (intern im Container)`);
+});
